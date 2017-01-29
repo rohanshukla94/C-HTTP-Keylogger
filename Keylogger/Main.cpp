92bb0ba7c535958c358094b44ef4e557d4ce7219
Rohan Shukla <shuklarohan420@gmail.com>
2017-01-29 17:09:06 +0530

    Update Main.cpp

--- Keylogger/Main.cpp
+++ Keylogger/Main.cpp
@@ -6,17 +6,6 @@
 You'll die as you lived in a flash of the blade in a corner forgotten the way.
 */
 
-/*
-No one deserves better treatment than others, or more respect. 
-No one is inferior or superior. Everyone is nothing. No soul is superior. 
-All men are created equal. All people are equal to bugs. All inherit an equal soul. 
-No one deserves special treatment. No one deserves anything more than anything else. 
-It all cancels out, and leaves life to be just nothing. It's true.
-Google: Truth Contest for the truth of life.﻿
-
-*/
-
-
 
 using namespace std;
 
